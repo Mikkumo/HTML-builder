a89e845698c991639a5d2cdec9f386f2d4df54e9
Lissa <Mikkumo@yandex.ru>
2022-05-20 20:38:54 +0400

    feat: completed 2 task

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout, exit } = require('process');
+const way = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('    Enter text: \n');
+
+stdin.on('data', (data) => {
+  if (data.toString().trim().toLowerCase() === 'exit') {
+    closeFunc();
+  } 
+  way.write(data);
+});
+
+process.on('', closeFunc);
+
+function closeFunc() {
+  stdout.write('    Goodbay!');
+  exit();
+}
